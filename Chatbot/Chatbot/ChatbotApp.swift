37cfba230191e47416624bde9f1374cff2c8b6e8
aaron <4142467+theaaron@users.noreply.github.com>
2024-09-11 21:57:38 -0500

    initializing new project

---
+++ Chatbot/Chatbot/ChatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatbotApp.swift
+//  Chatbot
+//
+//  Created by aaron on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
