c60fa685d51910180b45d6f36f39888ff65dbaa9
aaron <4142467+theaaron@users.noreply.github.com>
2024-09-12 19:20:25 -0500

    built barebones UI

--- Chatbot/Chatbot/ContentView.swift
+++ Chatbot/Chatbot/ContentView.swift
@@ -7,16 +7,40 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+
+    @StateObject var messageVM = MessageViewModel()
+    @State var userPrompt: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Text(messageVM.prompt)
+            Text(messageVM.aiResponse)
+            Spacer()
+                HStack {
+                    TextField("Hello Gemini", text: $userPrompt)
+                        .lineLimit(5)
+                        .onSubmit {
+                            updateResponse()
+                        }
+                    Button("Send") {
+                        updateResponse()
+                    }
+                }
         }
         .padding()
     }
+    
+    func updateResponse() {
+        messageVM.prompt = userPrompt
+        messageVM.generateResponse(userPrompt: userPrompt)
+        userPrompt = ""
+    }
+    
+
+    
 }
 
 #Preview {
