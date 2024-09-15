1c4386e7387db58df31111cc905bc8081b1b3f8c
aaron <4142467+theaaron@users.noreply.github.com>
2024-09-15 18:34:06 -0500

    Added startup view for no messages.

--- Chatbot/Chatbot/ContentView.swift
+++ Chatbot/Chatbot/ContentView.swift
@@ -9,24 +9,31 @@ import SwiftUI
 
 
 struct ContentView: View {
-
+    let gradColors: [Color] = [Color(NSColor(red: 0.1, green: 0.05, blue: 0.5, alpha: 1.0)),
+                               Color(NSColor(red: 0.3, green: 0.05, blue: 0.5, alpha: 1.0))]
+    let globeColors: [Color] = [Color(NSColor(red: 0.2, green: 0.1, blue: 0.9, alpha: 1.0)),
+                               Color(NSColor(red: 0.6, green: 0.1, blue: 0.9, alpha: 1.0))]
+    
     @StateObject var messageVM = MessageViewModel()
     @State var userPrompt: String = ""
     
     var body: some View {
         VStack {
             Spacer()
-            Text(messageVM.prompt)
-            Text(messageVM.aiResponse)
+            ConversationView(messageVM: messageVM, gradColors: gradColors, globeColors: globeColors, prompt: userPrompt)
             Spacer()
                 HStack {
                     TextField("Hello Gemini", text: $userPrompt)
                         .lineLimit(5)
                         .onSubmit {
-                            updateResponse()
+                            if userPrompt != "" {
+                                updateResponse()
+                            }
                         }
                     Button("Send") {
-                        updateResponse()
+                        if userPrompt != "" {
+                            updateResponse()
+                        }
                     }
                 }
         }
@@ -38,9 +45,31 @@ struct ContentView: View {
         messageVM.generateResponse(userPrompt: userPrompt)
         userPrompt = ""
     }
-    
+}
 
-    
+struct ConversationView: View {
+    let messageVM: MessageViewModel
+    let gradColors: [Color]
+    let globeColors: [Color]
+    let prompt: String
+    var body: some View {
+        VStack {
+            if messageVM.aiResponse == "" {
+                Image(systemName: "globe")
+                    .font(.largeTitle)
+                    .foregroundStyle(LinearGradient(colors: globeColors, startPoint: .bottom, endPoint: .top))
+            } else {
+                Text(messageVM.prompt)
+                    .padding()
+                    .cornerRadius(10)
+                    .background()
+                Text(messageVM.aiResponse)
+                    .padding()
+                    .background(LinearGradient(colors: gradColors, startPoint: .bottomLeading, endPoint: .topTrailing))
+            }
+            
+        }
+    }
 }
 
 #Preview {
