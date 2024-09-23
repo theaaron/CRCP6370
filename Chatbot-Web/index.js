f1be11879e0e4003d1fea5150e830454ef532aa4
aaron <4142467+theaaron@users.noreply.github.com>
2024-09-23 13:41:18 -0500

    adjusted system instructions

--- Chatbot-Web/index.js
+++ Chatbot-Web/index.js
@@ -2,15 +2,27 @@ document.getElementById("send-button").addEventListener("click", sendMessage);
 
 async function sendMessage() {
   const userInput = document.getElementById("user-input").value;
-  console.log(userInput);
+  document.getElementById("user-input").value = "";
   let promptEle = document.querySelector("#prompt");
   promptEle.textContent = userInput;
   promptEle.style.visibility = "visible";
 
-  const apiKey = "key";
+  let avatarEle = document.querySelector("#pic");
+  avatarEle.style.visibility = "visible";
+
+  let responseEle = document.querySelector("#response");
+  responseEle.style.visibility = "visible";
+  responseEle.textContent = "...";
+
+  const apiKey = API_KEY;
   const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`;
 
   const data = {
+    system_instruction: {
+      parts: {
+        text: "You are Vegeta from Dragon Ball Z. Respond like him.",
+      },
+    },
     contents: [
       {
         parts: [
@@ -31,8 +43,9 @@ async function sendMessage() {
   });
 
   const responseData = await response.json();
-  const unrwappedResponse = responseData.candidates[0].content.parts[0].text;
-  let responseEle = document.querySelector("#response");
-  responseEle.textContent = unrwappedResponse;
-  responseEle.style.visibility = "visible";
+  console.log(responseData);
+  const unwrappedResponse = responseData.candidates[0].content.parts[0].text;
+
+  responseEle.textContent = unwrappedResponse;
+  // responseEle.style.visibility = "visible";
 }
